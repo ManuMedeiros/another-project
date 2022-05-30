51c2f97f2c293262f1b8fbbb1aefb908a6fd0efe
Felipe Paulino <felipepaulino@frwk.com.br>
2022-05-30 16:14:06 -0300

    atualizacao

---
+++ src/pages/personagens/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+import React from "react";
+import * as S from "./styles";
+
+const Personagens: NextPage = () => {
+    return (
+        <></>
+    )
+}
+
+export default Personagens;
---
+++ src/utils/index.tsx
@@ -0,0 +1,56 @@
+export const personagens = [
+    {
+        nome: 'Kouichi Sakakibara',
+        img: 'http://2.bp.blogspot.com/-2392_21Z-ko/UQQhT6MdsCI/AAAAAAAAD_A/qmaXJfwybRA/s400/FG7Shock27hr.jpg',
+        descricao: "Ele acaba de se mudar para Yomiyama, e ele mal chega e fica doente,indo parar no hospital.Lá ele conhece Izumi,Tomohiko e Yukari,que vão 'prestar apoio' a ele,já que serão companheiros de classe.Ao ir a escola,ele percebe que seus colegas lhe escondem algo,enrolando sempre que vêm que soltaram algo que não deviam.No mesmo dia que conheceu os 3,Kouichi também conheceu Misaki Mei,que estava indo ao necrotério com uma boneca nas mãos.Na escola ele a reconhece na sala de aula,ela está na última fileira na última carteira,que é a mais antiga(tá toda rabiscada e desgastada),ele passou a conversar com ela sempre que a via,mesmo os outros alertando que ele não deveria.(...)Após ele entender que Mei realmente existe e o porquê dos outros fingirem dela não existir,Juntos eles passam a procurar um meio de parar a calamidade,junto com outros que acabam se juntando a eles.No geral,ele não demonstra muitos seus sentimentos,é tímido,determinado,etc."
+        
+    },
+    {
+        nome: 'Mei Misaki ',
+        img: 'http://3.bp.blogspot.com/-gqiw704vidY/UQQhYZoHWMI/AAAAAAAAD_I/cmxA1e3Gsmk/s400/Mei20.jpg',
+        descricao: "Misaki usa um tapa olho,esse tapa olho esconde um olho artificial(um olho de boneca feito especialmente por sua 'mãe')na cor verde.O olho que fica sem o tapa olho é vermelho.Misaki é bem fria,tem poucas expressões,acredito eu que ela ficou assim depois da morte de sua irmã gêmea,talvez.Mei acaba se tornando amiga de Kouichi,passando grande parte do tempo com ele.Na escola,Mei aceita se passar pela aluna extra e ser excluída do mundo,ela diz 'prefiro ser a ignorada'(é por ai '-'),referindo-se que ela prefere ser a ignorada a ignorar alguém.Seu olho de boneca é escondido por um motivo,ele pode 'ver a morte',a pessoa que 'está com a morte em torno de si' possui uma cor que apenas o olho de boneca pode ver.Sendo assim,Misaki é a única que sabe quem é a pessoa extra.(pessoa extra é o morto que está entre os vivos)",
+        
+    },
+    {
+        nome: 'Yūya Mochizuki',
+        img: 'http://1.bp.blogspot.com/-ZU0KANPl3wU/UQQhdpZUEuI/AAAAAAAAD_Q/J-lH87WXYhc/s400/Y%C5%ABya+Mochizuki+%5B+cantinho+da+drw%5D.jpg',
+        descricao: "Ele também ajuda a parar a calamidade,é integrante do clube de artes e é ele quem conserta a fita que Naoya quebra.",
+        
+    },
+    {
+        nome: 'Yukari Sakuragi',
+        img: 'http://2.bp.blogspot.com/-fAPprl1nFKE/UQQhiDMnyCI/AAAAAAAAD_Y/LqMugO5iLME/s400/yukari.jpg',
+        descricao: "Foi a morte de maio,ela morre após tropeçar na escada e o guarda-chuva atravessar-lhe a garganta.",
+        
+    },
+    {
+        nome: 'Izumi Akazawa',
+        img: 'http://1.bp.blogspot.com/-70NjKMIVl2g/UQQhmUcpKBI/AAAAAAAAD_g/PUzy0AA_l-I/s400/510068-screenshot061.jpg',
+        descricao: "Chefe das contra-medidas,gosta do Kouichi,não gosta de Mei por achar que foi ela quem quebrou o 'selo' da maldição,de todos,ela foi a última a morrer.",
+        
+    },
+    {
+        nome: 'Naoya Teshigawara',
+        img: 'http://4.bp.blogspot.com/-4gSgs3jTLN0/UQQht3ZVopI/AAAAAAAAD_o/66V5qecW8YE/s400/4758-2060272737.png',
+        descricao: "É bem extrovertido e adora esportes. Ele se junta a Mei e Kouichi para ajudar a parar a calamidade.",
+        
+    },
+    {
+        nome: 'Tatsuji Chibiki ',
+        img: 'http://3.bp.blogspot.com/-QNK2mYuuf8A/UQQh1_AzOZI/AAAAAAAAD_w/L75YkIcsBms/s400/Tatsuji+Chibiki++%5Bcantinho+da+drw%5D.png',
+        descricao: "Bibliotecário,ele foi o professor da classe a qual começou a calamidade,e ajuda no que pode os alunos que estão passando por uma nova calamidade.Tatsuji também esclarece algumas coisas em relação a maldição.",
+        
+    },
+    {
+        nome: 'Sanae Mizuno',
+        img: 'http://4.bp.blogspot.com/-tP2J5yQjMd4/UQQh71isNvI/AAAAAAAAD_4/BXFFR9quryo/s320/Zerochan-mizuno-sanae-952423.jpg',
+        descricao: "Enfermeira do hospital onde Kouichi ficou internado,eles acabam se tornado amigos.Ela o ajuda a descobrir quem Mei estava visitando aquele dia no necrotério.Sanae foi uma das vítimas da calamidade,sendo a morte de Junho.",
+        
+    },
+    {
+        nome: 'Reiko Mikami',
+        img: 'http://2.bp.blogspot.com/-ZKNhmUveG90/UQQiA4xix5I/AAAAAAAAEAA/0rMQIKyZG3g/s400/Reiko9.jpg',
+        descricao: "Tia de Kouchi,embora ela não se pareça em nada com a professora auxiliar da sala 3-3,é ela.Isso só é revelado no último episódio do anime(o normal,o OVA eu não sei '-' )",
+        
+    },
+]
